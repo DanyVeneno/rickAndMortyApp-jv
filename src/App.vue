2a310e6232cfe72ce039cec78e412704d8fed90e
Daniel Vallejo <yehiibhiidigital@gmail.com>
2022-06-05 17:27:34 -0500

    Rick 6 Morty Proyect with Vuejs

---
+++ src/App.vue
@@ -0,0 +1,71 @@
+<template>
+  <div>
+    <h1>Rick & Morty</h1>
+
+    <FilterByStatus />
+    <FilterByName />
+    <ListCharacters />
+
+  </div>
+</template>
+
+<script>
+import ListCharacters from '@/components/ListCharacters'
+import FilterByStatus from '@/components/FilterByStatus'
+import FilterByName from '@/components/FilterByName'
+
+export default {
+  name: 'App',
+  components: {
+   ListCharacters,
+   FilterByStatus,
+   FilterByName
+    
+  }
+}
+</script>
+
+<style lang="scss">
+
+
+:root{
+  --background-body:#24282f;
+  --background-card:#3c3e44
+  --text-white:#efefef;
+  --text-gray:#969696;
+  --text-orange:rgb(160, 247, 78);
+}
+*{
+box-sizing:border-box;
+margin:0;
+padding:0;
+
+}
+body{
+  background-color: var(--background-body);
+  color:var(--text-white);
+  font-family:'Segoe UI','Segoe UI', Tahoma, Geneva, Verdana, sans-serif;
+
+}
+h1{
+  color:whitesmoke;
+  margin-bottom: 3rem;
+  text-align: center;
+  margin-top:5rem;
+
+}
+.container{
+  width:70%;
+  max-width: 80%;
+  margin:5rem;
+}
+img{
+  width:100%;
+
+}
+
+
+
+
+
+</style>
---
+++ src/components/CardCharacter.vue
@@ -0,0 +1,99 @@
+<template>
+  <div class="character">
+      <img :src="character.image" :alt="character.name">
+
+      <div class="character_info">
+          <h4>{{ character.name }}</h4>
+          <div class="status">
+              <span
+                  :class="
+                    character.status == 'Alive' ? 'alive' :
+                    character.status == 'Dead' ? 'dead' :
+                    'default'
+                  
+                  "
+                  >
+              </span>
+              <span>{{character.status}} - {{character.species}}</span>
+          </div>
+          <div class="origin">
+              <span>
+                  Origin:
+                  {{character.origin.name}}
+              </span>
+          </div>
+
+
+
+      </div>
+  </div>
+</template>
+
+<script>
+export default {
+    props:['character']
+
+}
+</script>
+
+<style>
+.character{
+  background-color: var(--background-card);
+  border-radius: 20px;
+  box-shadow: 0 0 10px 1px var(--background-body);
+  overflow: hidden;
+  cursor: pointer;
+  transition:transform 200ms ease-in-out;
+  height:100%;
+}
+  .character:hover {
+    transform: scale(1.05);
+  } 
+  
+  h4 {
+      color: var(--text-orange);
+    }
+  span{
+    color: var(--text-gray);
+  }
+  h4{
+    margin-bottom: 0.5rem;
+  }
+  .character_info {
+    padding:1.5rem;
+  }
+    .status {
+      display: flex;
+      align-items: center;
+      margin-bottom: 0.5rem;
+    }
+  span {
+      color: var(--text-gray);
+      }
+    span:first-child {
+          width: 10px;
+          height: 10px;
+          border-radius: 50%;
+          margin-left: 0.5rem;
+        }
+
+      
+ .alive{
+   background-color:greenyellow;
+ }
+ .dead{
+   background-color: tomato;
+ }
+.default{
+  background-color: whitesmoke;
+ }
+
+
+.origen {
+  margin-bottom:0.5rem;
+}
+  
+
+
+
+</style>
